338bd88d5d625b5781e15928b6661083931605bf
agustus.a <agustus.a@fiserv.com>
2024-02-13 16:10:34 +0530

    Adding ThirdFragment Commit

--- app/src/main/java/com/example/navigation/SecondFragment.kt
+++ app/src/main/java/com/example/navigation/SecondFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.navigation.Navigation
 
@@ -17,7 +18,9 @@ class SecondFragment : Fragment() {
         // Inflate the layout for this fragment
         val view =  inflater.inflate(R.layout.fragment_second, container, false)
         val textView = view.findViewById<TextView>(R.id.home_msg)
+        val btn = view.findViewById<Button>(R.id.button)
         textView.setOnClickListener{ Navigation.findNavController(view).navigate(R.id.action_secondFragment_to_firstFragment)}
+        btn.setOnClickListener{ Navigation.findNavController(view).navigate(R.id.action_secondFragment_to_thirdFragment)}
         return view
     }
 }
